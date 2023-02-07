f3110b184999d4c5161b732a86d2c0653702ba3d
Musa KIRKKESELİ <musakirkeseli@gmail.com>
2023-02-07 06:59:56 +0300

    Added photo swapping feature

--- BaseProject/ViewController.swift
+++ BaseProject/ViewController.swift
@@ -12,45 +12,32 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myImageView: UIImageView!
     
+    let istanbulPhoto1 = UIImage(named: "istanbul.jpeg")
+    let istanbulPhoto2 = UIImage(named: "istanbul_kiz_kulesi.jpeg")
     
-//    lazy var gradient: CAGradientLayer = {
-//        let gradient = CAGradientLayer()
-//        gradient.type = .axial
-//        gradient.colors = [
-//            UIColor.red.cgColor,
-//            UIColor.purple.cgColor,
-//            UIColor.cyan.cgColor
-//        ]
-//        gradient.locations = [0, 0.25, 1]
-//        return gradient
-//    }()
-    
-    let colorTop = UIColor.white.cgColor
-    let colorBottom = UIColor(red: 0.941 , green: 0.818, blue: .zero, alpha: 1.0).cgColor
     let gradientLayer = CAGradientLayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-//        gradient.frame = self.view.bounds
-//        self.view.layer.addSublayer(gradient)
         
         gradientLayer.frame = self.view.bounds
         gradientLayer.colors = [UIColor.red.cgColor,
                                 UIColor.purple.cgColor,
                                 UIColor.cyan.cgColor]
         self.view.layer.insertSublayer(gradientLayer, at: 0)
-        
+        myImageView.image = istanbulPhoto1
         myLabel.text = "İstanbul'u dinliyorum, gözlerim kapalı"
     }
     
     @IBAction func onTap(_ sender: Any) {
         if myLabel.text == "İstanbul'u dinliyorum, gözlerim kapalı"{
             myLabel.text = "Ne güzel İstanbul be!"
+            myImageView.image = istanbulPhoto2
         }else{
             myLabel.text = "İstanbul'u dinliyorum, gözlerim kapalı"
+            myImageView.image = istanbulPhoto1
         }
     }
     
